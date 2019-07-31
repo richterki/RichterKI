5180cf2088e545c615b05a8124c454e5ddd8f6c7
Diacrafter20 <info@diacrafter20.de>
2019-07-31 11:38:54 +0200

    Datenbank fertig

---
+++ database/create_database.py
@@ -0,0 +1,94 @@
+
+# ----Example Python Program to create tables in disk file based SQLite databases----
+
+
+# Import the sqlite3 module
+
+import sqlite3
+
+
+# Create a database connection to a disk file based database
+
+connectionObject = sqlite3.connect("../db/test.db")
+
+
+# Obtain a cursor object
+
+cursorObject = connectionObject.cursor()
+
+
+# Drop any existing table with the same name
+
+# dropTable = "drop table temperature"
+
+# cursorObject.execute(dropTable)
+
+
+# Create a table in the disk file based database
+
+createTable_fall = "CREATE TABLE IF NOT EXISTS fall(fall_id int, stadt_id int, urteil varchar, schlagwoerter varchar(3,1))"
+cursorObject.execute(createTable_fall)
+
+createTable_fall_infos = "CREATE TABLE IF NOT EXISTS fall_infos(fall_id int, protokoll varchar, urteil varchar(3,1))"
+cursorObject.execute(createTable_fall_infos)
+
+createTable_namen = "CREATE TABLE IF NOT EXISTS namen(fall_id int, klaeger varchar, angeklagter varchar, richter varchar(3,1))"
+cursorObject.execute(createTable_namen)
+
+createTable_gesetzbuecher_liste = "CREATE TABLE IF NOT EXISTS gesetzbuecher_liste(buch_id int, name varchar, justiz_bereich varchar(3,1))"
+cursorObject.execute(createTable_gesetzbuecher_liste)
+
+createTable_gesetzbuecher = "CREATE TABLE IF NOT EXISTS gesetzbuecher(buch_id int, paragraphen int, absatz varchar, inhalt varchar(3,1))"
+cursorObject.execute(createTable_gesetzbuecher)
+
+# Insert EOD stats into the reports table
+
+# insertValues = "INSERT INTO temperature values(1,40.1)"
+
+# cursorObject.execute(insertValues)
+
+
+# insertValues = "INSERT INTO temperature values(2,65.4)"
+
+# cursorObject.execute(insertValues)
+
+
+# Select from the temperature table
+
+queryTable_fall = "SELECT * from fall"
+queryResults_fall = cursorObject.execute(queryTable_fall)
+
+queryTable_fall_infos = "SELECT * from fall_infos"
+queryResults_fall_infos = cursorObject.execute(queryTable_fall_infos)
+
+queryTable_namen = "SELECT * from namen"
+queryResults_namen = cursorObject.execute(queryTable_namen)
+
+queryTable_gesetzbuecher_liste = "SELECT * from gesetzbuecher_liste"
+queryResults_gesetzbuecher_liste = cursorObject.execute(queryTable_gesetzbuecher_liste)
+
+queryTable_gesetzbuecher = "SELECT * from gesetzbuecher"
+queryResults_gesetzbuecher = cursorObject.execute(queryTable_gesetzbuecher)
+
+
+# Print the Temperature records
+
+print("(CityId, Temperature)")
+
+for result in queryResults_fall:
+    print(result)
+
+for result in queryResults_fall_infos:
+    print(result)
+
+for result in queryResults_namen:
+    print(result)
+
+for result in queryResults_gesetzbuecher_liste:
+    print(result)
+
+for result in queryResults_gesetzbuecher:
+    print(result)
+
+
+connectionObject.close()
