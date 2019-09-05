5035784e7391952efa358b1b45703863b582f9bd
Diacrafter20 <info@diacrafter20.de>
2019-09-05 17:38:40 +0200

    Datenbanken ganz fertig

--- database.py
+++ database.py
@@ -8,30 +8,30 @@ def create_connection(db_file):
     :param db_file: database file
     :return: Connection object or None
     """
+    conn = None
     try:
         conn = sqlite3.connect(db_file)
-        return conn
     except Error as e:
         print(e)
 
-    return None
+    return conn
 
 
-def create_project(conn, project):
+def create_fall(conn, fall):
     """
     Create a new project into the projects table
     :param conn:
     :param project:
     :return: project id
     """
-    sql = ''' INSERT INTO projects(name,begin_date,end_date)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO fall(fall_id,stadt_id,urteil,schlagwoerter)
+              VALUES(?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, fall)
     return cur.lastrowid
 
 
-def create_task(conn, task):
+def create_fall_infos(conn, fall_infos):
     """
     Create a new task
     :param conn:
@@ -39,32 +39,94 @@ def create_task(conn, task):
     :return:
     """
 
-    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
-              VALUES(?,?,?,?,?,?) '''
+    sql = ''' INSERT INTO fall_infos(fall_id,protokoll,urteil)
+              VALUES(?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, task)
+    cur.execute(sql, fall_infos)
+    return cur.lastrowid
+
+
+def create_gesetzbuecher(conn, gesetzbuecher):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO gesetzbuecher(buch_id,paragraphen,absatz,inhalt)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, gesetzbuecher)
+    return cur.lastrowid
+
+
+def create_gesetzbuecher_liste(conn, gesetzbuecher_liste):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO gesetzbuecher_liste(buch_id,name,justiz_bereich)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, gesetzbuecher_liste)
+    return cur.lastrowid
+
+
+def create_namen(conn, namen):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO namen(fall_id,klaeger,angeklagter,richter)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, namen)
     return cur.lastrowid
 
 
 def main():
-    database = "db/pythonsqlite.db"
+    database = r"D:\\CODE\\Python\\RichterKI\\db\\test.db"
 
     # create a database connection
     conn = create_connection(database)
     with conn:
-        # create a new project
-        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30')
-        project_id = create_project(conn, project)
+        # fall
+        print('===FALL===')
+        fall = (int(input('fall_id >> ')), int(input('stadt_id >> ')),
+                input('urteil >> '), input('schlagwoerter >> '))
+        create_fall(conn, fall)
 
-        # tasks
-        task_1 = ('Analyze the requirements of the app', 1,
-                  1, project_id, '2015-01-01', '2015-01-02')
-        task_2 = ('Confirm with user about the top requirements',
-                  1, 1, project_id, '2015-01-03', '2015-01-05')
+        # fall_infos
+        print('===FALL INFOS===')
+        fall_infos = (int(input('fall_id >> ')), input('protokoll >> '), input('urteil >> '))
+        create_fall_infos(conn, fall_infos)
 
-        # create tasks
-        create_task(conn, task_1)
-        create_task(conn, task_2)
+        # gesetzbuecher
+        print('===GESETZBÜCHER===')
+        gesetzbuecher = (int(input('buch_id >> ')), int(input('paragraphen >> ')),
+                         input('absatz >> '), input('inhalt >> '))
+        create_gesetzbuecher(conn, gesetzbuecher)
+
+        # gesetzbuecher_liste
+        print('===GESETZBÜCHER LISTE===')
+        gesetzbuecher_liste = (int(input('buch_id >> ')), input(
+            'name >> '), input('justiz_bereich >> '))
+        create_gesetzbuecher_liste(conn, gesetzbuecher_liste)
+
+        # namen
+        print('===NAMEN===')
+        namen = (int(input('buch_id >> ')), input('klaeger >> '),
+                 input('angeklagter >> '), input('richter >> '))
+        create_namen(conn, namen)
+
+        print('Datenbank wurde beschrieben')
 
 
 if __name__ == '__main__':
